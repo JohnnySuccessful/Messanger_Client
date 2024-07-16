af2cfd4be4433c262dd9432fca735e4cc63d1749
John Smith <pawarker@gmail.com>
2024-07-16 13:49:56 -0700

    Some changes

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,134 +1,120 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import styled from "styled-components";
-import {Link, useNavigate} from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Logo from "../assets/logo.svg";
-import {toast, ToastContainer} from "react-toastify";
+import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 export default function Register() {
-  const navigate = useNavigate();
-  const toastOptions = {
-    position: "bottom-right",
-    autoClose: 8000,
-    pauseOnHover: true,
-    draggable: true,
-    theme: "dark",
-  };
-  const [values, setValues] = useState({
-    username: "",
-    email: "",
-    password: "",
-    confirmPassword: "",
-  });
+    const navigate = useNavigate();
+    const toastOptions = {
+        position: "bottom-right",
+        autoClose: 8000,
+        pauseOnHover: true,
+        draggable: true,
+        theme: "dark",
+    };
+    const [values, setValues] = useState({
+        username: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+    });
 
-  useEffect(() => {
-    if (localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)) {
-      navigate("/");
-    }
-  }, []);
+    useEffect(() => {
+        if (localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)) {
+            navigate("/");
+        }
+    }, []);
 
-  const handleChange = (event) => {
-    setValues({ ...values, [event.target.name]: event.target.value });
-  };
+    const handleChange = (event) => {
+        setValues({ ...values, [event.target.name]: event.target.value });
+    };
 
-  const handleValidation = () => {
-    const { password, confirmPassword, username, email } = values;
-    if (password !== confirmPassword) {
-      toast.error(
-        "Password and confirm password should be same.",
-        toastOptions
-      );
-      return false;
-    } else if (username.length < 3) {
-      toast.error(
-        "Username should be greater than 3 characters.",
-        toastOptions
-      );
-      return false;
-    } else if (password.length < 8) {
-      toast.error(
-        "Password should be equal or greater than 8 characters.",
-        toastOptions
-      );
-      return false;
-    } else if (email === "") {
-      toast.error("Email is required.", toastOptions);
-      return false;
-    }
+    const handleValidation = () => {
+        const { password, confirmPassword, username, email } = values;
+        if (password !== confirmPassword) {
+            toast.error("Password and confirm password should be same.", toastOptions);
+            return false;
+        } else if (username.length < 3) {
+            toast.error("Username should be greater than 3 characters.", toastOptions);
+            return false;
+        } else if (password.length < 8) {
+            toast.error("Password should be equal or greater than 8 characters.", toastOptions);
+            return false;
+        } else if (email === "") {
+            toast.error("Email is required.", toastOptions);
+            return false;
+        }
 
-    return true;
-  };
+        return true;
+    };
 
     const handleSubmit = async (event) => {
         event.preventDefault();
         if (handleValidation()) {
             const { email, username, password } = values;
-            try {
-                const { data } = await axios.post(
-                    `${process.env.REACT_APP_API_URL}/api/auth/register`,
-                        username,
-                        email,
-                        password,
-                );
+            const { data } = await axios.post(`${process.env.REACT_APP_API_URL}/auth/register`, {
+                username,
+                email,
+                password,
+            });
 
-                if (data.status === false) {
-                    toast.error(data.msg, toastOptions);
-                }
-                if (data.status === true) {
-                    localStorage.setItem(
-                        process.env.REACT_APP_LOCALHOST_KEY,
-                        JSON.stringify(data.user)
-                    );
-                    navigate("/");
-                }
-            } catch (error) {
-                toast.error("Network error or server is not available", toastOptions);
+            if (data.status === false) {
+                toast.error(data.msg, toastOptions);
+            }
+            if (data.status === true) {
+                localStorage.setItem(
+                    process.env.REACT_APP_LOCALHOST_KEY,
+                    JSON.stringify(data.user)
+                );
+                navigate("/");
             }
         }
     };
 
     return (
-    <>
-      <FormContainer>
-        <form action="" onSubmit={(event) => handleSubmit(event)}>
-          <div className="brand">
-            <img src={Logo} alt="logo" />
-            <h1>snappy</h1>
-          </div>
-          <input
-            type="text"
-            placeholder="Username"
-            name="username"
-            onChange={(e) => handleChange(e)}
-          />
-          <input
-            type="email"
-            placeholder="Email"
-            name="email"
-            onChange={(e) => handleChange(e)}
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            name="password"
-            onChange={(e) => handleChange(e)}
-          />
-          <input
-            type="password"
-            placeholder="Confirm Password"
-            name="confirmPassword"
-            onChange={(e) => handleChange(e)}
-          />
-          <button type="submit">Create User</button>
-          <span>
-            Already have an account ? <Link to="/login">Login.</Link>
+        <>
+            <FormContainer>
+                <form action="" onSubmit={(event) => handleSubmit(event)}>
+                    <div className="brand">
+                        <img src={Logo} alt="logo" />
+                        <h1>snappy</h1>
+                    </div>
+                    <input
+                        type="text"
+                        placeholder="Username"
+                        name="username"
+                        onChange={(e) => handleChange(e)}
+                    />
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        name="email"
+                        onChange={(e) => handleChange(e)}
+                    />
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                        onChange={(e) => handleChange(e)}
+                    />
+                    <input
+                        type="password"
+                        placeholder="Confirm Password"
+                        name="confirmPassword"
+                        onChange={(e) => handleChange(e)}
+                    />
+                    <button type="submit">Create User</button>
+                    <span>
+            Already have an account? <Link to="/login">Login.</Link>
           </span>
-        </form>
-      </FormContainer>
-      <ToastContainer />
-    </>
-  );
+                </form>
+            </FormContainer>
+            <ToastContainer />
+        </>
+    );
 }
 
 const FormContainer = styled.div`
